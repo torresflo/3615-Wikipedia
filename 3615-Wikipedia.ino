41ef6ca7d5e8a3b8436edb2521dd463ba9cbdc07
Florian Torres <50424335+torresflo@users.noreply.github.com>
2026-06-26 22:22:05 +0200

    Auto indent sketch

--- 3615-Wikipedia.ino
+++ 3615-Wikipedia.ino
@@ -41,220 +41,221 @@ String serverPathPart2 = "&prop=extracts&exintro&explaintext";
 int currentStep = STEP_WIFI_LOADING;
 String stringEnteredByUser = "";
 
-JsonVariant findNestedKey(JsonObject obj, const char* key)
+JsonVariant findNestedKey(JsonObject obj, const char *key)
 {
-  JsonVariant foundObject = obj[key];
-  if (!foundObject.isNull())
-    return foundObject;
+    JsonVariant foundObject = obj[key];
+    if (!foundObject.isNull())
+        return foundObject;
 
-  for (JsonPair pair : obj)
-  {
-    JsonVariant nestedObject = findNestedKey(pair.value(), key);
-    if (!nestedObject.isNull())
-      return nestedObject;
-  }
-
-  return JsonVariant();
-}
-
-unsigned int displayVideotexLines(String& text, unsigned int startCharacter, unsigned int startLine, unsigned int endLine)
-{
-  if(startCharacter > text.length() - 1)
-    return text.length();
-
-  unsigned int currentStartChar = startCharacter;
-  unsigned int currentEndChar = currentStartChar;
-
-  for(unsigned int i = startLine; i <= endLine; ++i)
-  {
-    minitel.moveCursorXY(0, i);
-    for(unsigned int j = 0; j <= MINITEL_VIDEOTEXT_COLUMNS; ++j)
+    for (JsonPair pair : obj)
     {
-      unsigned int charIndex = currentStartChar + j;
-      if(charIndex >= text.length())
-      {
-        minitel.print(text.substring(currentStartChar));
-        return text.length();
-      }
-
-      if(text.charAt(charIndex) == ' ')
-      {
-        currentEndChar = charIndex;
-      }
+        JsonVariant nestedObject = findNestedKey(pair.value(), key);
+        if (!nestedObject.isNull())
+            return nestedObject;
     }
 
-    minitel.print(text.substring(currentStartChar, currentEndChar));
-    currentStartChar = currentEndChar + 1;
-  }
-  return currentStartChar;
+    return JsonVariant();
+}
+
+unsigned int displayVideotexLines(String &text, unsigned int startCharacter, unsigned int startLine, unsigned int endLine)
+{
+    if (startCharacter > text.length() - 1)
+        return text.length();
+
+    unsigned int currentStartChar = startCharacter;
+    unsigned int currentEndChar = currentStartChar;
+
+    for (unsigned int i = startLine; i <= endLine; ++i)
+    {
+        minitel.moveCursorXY(0, i);
+        for (unsigned int j = 0; j <= MINITEL_VIDEOTEXT_COLUMNS; ++j)
+        {
+            unsigned int charIndex = currentStartChar + j;
+            if (charIndex >= text.length())
+            {
+                minitel.print(text.substring(currentStartChar));
+                return text.length();
+            }
+
+            if (text.charAt(charIndex) == ' ')
+            {
+                currentEndChar = charIndex;
+            }
+        }
+
+        minitel.print(text.substring(currentStartChar, currentEndChar));
+        currentStartChar = currentEndChar + 1;
+    }
+    return currentStartChar;
 }
 
 void displayNewPage(String title)
 {
-  minitel.newScreen();
-  minitel.textMode();
-  minitel.noCursor();
-  minitel.newXY(1, 0);
-  minitel.attributs(CARACTERE_ROUGE);
-  minitel.print(title);
-  minitel.cancel();
-  minitel.moveCursorDown(1);
+    minitel.newScreen();
+    minitel.textMode();
+    minitel.noCursor();
+    minitel.newXY(1, 0);
+    minitel.attributs(CARACTERE_ROUGE);
+    minitel.print(title);
+    minitel.cancel();
+    minitel.moveCursorDown(1);
 
-  minitel.attributs(CARACTERE_ROUGE);
-  for (int i=1; i<=40; i++)
-  {
-    minitel.writeByte(0x7E);
-  }
+    minitel.attributs(CARACTERE_ROUGE);
+    for (int i = 1; i <= 40; i++)
+    {
+        minitel.writeByte(0x7E);
+    }
 
-  minitel.moveCursorReturn(0);
-  minitel.attributs(CARACTERE_BLANC);
+    minitel.moveCursorReturn(0);
+    minitel.attributs(CARACTERE_BLANC);
 }
 
 void displayPromptPage()
 {
-  currentStep = STEP_USER_INPUT;
+    currentStep = STEP_USER_INPUT;
 
-  stringEnteredByUser = "";
+    stringEnteredByUser = "";
 
-  displayNewPage("3615 Wikipedia");
-  minitel.println("What are you searching for?");
-  minitel.cursor();
+    displayNewPage("3615 Wikipedia");
+    minitel.println("What are you searching for?");
+    minitel.cursor();
 }
 
 void displayWikipediaPage(int pageSeparatorsIndex)
 {
-  displayNewPage("3615 Wikipedia");
-  minitel.attributs(CARACTERE_ROUGE);
-  minitel.println("Result for \"" + stringEnteredByUser + "\":");
-  minitel.attributs(CARACTERE_BLANC);
+    displayNewPage("3615 Wikipedia");
+    minitel.attributs(CARACTERE_ROUGE);
+    minitel.println("Result for \"" + stringEnteredByUser + "\":");
+    minitel.attributs(CARACTERE_BLANC);
 
-  if(pageSeparatorsIndex < 0)
-    pageSeparatorsIndex = 0;
-  if(pageSeparatorsIndex >= pageSeparators.size())
-    pageSeparatorsIndex = pageSeparators.size() - 1;
+    if (pageSeparatorsIndex < 0)
+        pageSeparatorsIndex = 0;
+    if (pageSeparatorsIndex >= pageSeparators.size())
+        pageSeparatorsIndex = pageSeparators.size() - 1;
 
-  if(wikipediaPage.length() == 0)
-  {
-    minitel.println("We found nothing :(");
-  }
-  else
-  {
-    unsigned int charIndex = pageSeparators.at(pageSeparatorsIndex);
-    unsigned int separatorIndex = displayVideotexLines(wikipediaPage, charIndex, 3, MINITEL_VIDEOTEXT_LINES);
-    if(separatorIndex < wikipediaPage.length() && pageSeparatorsIndex + 1 >= pageSeparators.size())
+    if (wikipediaPage.length() == 0)
     {
-      pageSeparators.push_back(separatorIndex);
+        minitel.println("We found nothing :(");
     }
-    currentPageSeparatorsIndex = pageSeparatorsIndex;
-  }
+    else
+    {
+        unsigned int charIndex = pageSeparators.at(pageSeparatorsIndex);
+        unsigned int separatorIndex = displayVideotexLines(wikipediaPage, charIndex, 3, MINITEL_VIDEOTEXT_LINES);
+        if (separatorIndex < wikipediaPage.length() && pageSeparatorsIndex + 1 >= pageSeparators.size())
+        {
+            pageSeparators.push_back(separatorIndex);
+        }
+        currentPageSeparatorsIndex = pageSeparatorsIndex;
+    }
 
-  currentStep = STEP_DISPLAY_RESULT;
+    currentStep = STEP_DISPLAY_RESULT;
 }
 
 void setup()
 {
-  minitel.changeSpeed(minitel.searchSpeed());
-  minitel.echo(false);
-  minitel.smallMode();
-  minitel.extendedKeyboard();
+    minitel.changeSpeed(minitel.searchSpeed());
+    minitel.echo(false);
+    minitel.smallMode();
+    minitel.extendedKeyboard();
 
-  currentStep = STEP_WIFI_LOADING;
-  displayNewPage("Loading...");
+    currentStep = STEP_WIFI_LOADING;
+    displayNewPage("Loading...");
 
-  minitel.print("Connection to Wi-fi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    minitel.print(".");
-  }
-  minitel.println("DONE");
+    minitel.print("Connection to Wi-fi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        minitel.print(".");
+    }
+    minitel.println("DONE");
 
-  displayPromptPage();
+    displayPromptPage();
 }
 
 void loop()
 {
-  if(currentStep == STEP_USER_INPUT)
-  {
-    unsigned long key = minitel.getKeyCode(false);
-    if(key != 0)
+    if (currentStep == STEP_USER_INPUT)
     {
-      switch(key)
-      {
-        case TOUCHE_FLECHE_GAUCHE:
-          break;
+        unsigned long key = minitel.getKeyCode(false);
+        if (key != 0)
+        {
+            switch (key)
+            {
+            case TOUCHE_FLECHE_GAUCHE:
+                break;
 
-        case TOUCHE_FLECHE_DROITE:
-          break;
+            case TOUCHE_FLECHE_DROITE:
+                break;
 
-        case ENVOI:
-          if(stringEnteredByUser.length() > 0)
-          {
-            currentStep = STEP_HTTP_REQUEST;
-          }
-          break;
-      
-        default:
-          String keyAsString = minitel.getString(key);
-          minitel.print(keyAsString);
-          stringEnteredByUser += keyAsString;
-          break;
-      }
+            case ENVOI:
+                if (stringEnteredByUser.length() > 0)
+                {
+                    currentStep = STEP_HTTP_REQUEST;
+                }
+                break;
+
+            default:
+                String keyAsString = minitel.getString(key);
+                minitel.print(keyAsString);
+                stringEnteredByUser += keyAsString;
+                break;
+            }
+        }
     }
-  }
-  
-  if(currentStep == STEP_HTTP_REQUEST)
-  {
-    displayNewPage("3615 Wikipedia");
-    minitel.print("Performing search on Wikipedia...");
 
-    if(WiFi.status() == WL_CONNECTED)
+    if (currentStep == STEP_HTTP_REQUEST)
     {
-      HTTPClient client;
-      String serverPath = serverPathPart1 + stringEnteredByUser + serverPathPart2;
+        displayNewPage("3615 Wikipedia");
+        minitel.print("Performing search on Wikipedia...");
 
-      client.begin(serverPath.c_str());
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            HTTPClient client;
+            String serverPath = serverPathPart1 + stringEnteredByUser + serverPathPart2;
 
-      int httpResponseCode = client.GET();
+            client.begin(serverPath.c_str());
 
-      if(httpResponseCode > 0)
-      {
-        String result = client.getString();
-        
-        deserializeJson(jsonDocument, result);
-        wikipediaPage = findNestedKey(jsonDocument.as<JsonObject>(), "extract").as<String>();
-        
-        pageSeparators.clear();
-        pageSeparators.push_back(0);
-        currentPageSeparatorsIndex = 0;
+            int httpResponseCode = client.GET();
 
-        displayWikipediaPage(currentPageSeparatorsIndex);
-      }
+            if (httpResponseCode > 0)
+            {
+                String result = client.getString();
+
+                deserializeJson(jsonDocument, result);
+                wikipediaPage = findNestedKey(jsonDocument.as<JsonObject>(), "extract").as<String>();
+
+                pageSeparators.clear();
+                pageSeparators.push_back(0);
+                currentPageSeparatorsIndex = 0;
+
+                displayWikipediaPage(currentPageSeparatorsIndex);
+            }
+        }
     }
-  }
 
-  if(currentStep == STEP_DISPLAY_RESULT)
-  {
-    unsigned long key = minitel.getKeyCode(false);
-    if(key != 0)
+    if (currentStep == STEP_DISPLAY_RESULT)
     {
-      switch(key)
-      {
-        case SOMMAIRE:
-          displayPromptPage();
-          break;
+        unsigned long key = minitel.getKeyCode(false);
+        if (key != 0)
+        {
+            switch (key)
+            {
+            case SOMMAIRE:
+                displayPromptPage();
+                break;
 
-        case SUITE:
-          displayWikipediaPage(currentPageSeparatorsIndex + 1);
-          break;
+            case SUITE:
+                displayWikipediaPage(currentPageSeparatorsIndex + 1);
+                break;
 
-        case RETOUR:
-          displayWikipediaPage(currentPageSeparatorsIndex - 1);
-          break;
+            case RETOUR:
+                displayWikipediaPage(currentPageSeparatorsIndex - 1);
+                break;
 
-        default:
-          break;
-      }
+            default:
+                break;
+            }
+        }
     }
-  }
 }
